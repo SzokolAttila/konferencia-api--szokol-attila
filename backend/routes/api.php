34ac1833bf964db1d7881fe2437a6f1714d23a7a
SzokolAttila <szokol.ati2005@gmail.com>
2024-12-09 12:55:42 +0000

    created Routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Http\Controllers\RegistrationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get("registrations", [RegistrationController::class, "index"])->name("registrations.index");
+Route::get("registrations/{registrations}", [RegistrationController::class, "show"])->name("registrations.show")->whereNumber("registrations");
+Route::delete("registrations/{registrations}", [RegistrationController::class, "destroy"])->name("registrations.destroy")->whereNumber("registrations");
+Route::get("registrations/count", [RegistrationController::class, "count"])->name("registrations.count");
+
