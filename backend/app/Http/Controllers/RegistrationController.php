eec61df07db91dcc9ee770898e387dc76c0e0840
SzokolAttila <szokol.ati2005@gmail.com>
2024-12-09 13:20:49 +0000

    finished endpoints

--- backend/app/Http/Controllers/RegistrationController.php
+++ backend/app/Http/Controllers/RegistrationController.php
@@ -4,13 +4,47 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\RegistrationResource;
 use App\Models\Registration;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Http\Response;
 
 class RegistrationController extends Controller
 {
-    public function index() : JsonResource
+    public function index(Request $request) : JsonResource
     {
-        return RegistrationResource::collection(Registration::all());
+        $registrations = Registration::query();
+        $orderBy = $request->query("orderBy");
+        $order = $request->query("order");
+        if ($orderBy == "name"){
+            if ($order == "asc")
+                $registrations->orderBy("name");
+            else if($order == "desc")
+                $registrations->orderByDesc("name");
+        }
+        else if ($orderBy == "date"){
+            if ($order == "asc")
+                $registrations->orderBy("date");
+            else if ($order == "desc")
+                $registrations->orderByDesc("date");
+        }
+        return RegistrationResource::collection($registrations->get());
+    }
+
+    public function show(int $registrations) : JsonResource
+    {
+        return new RegistrationResource(Registration::findOrFail($registrations));
+    }
+
+    public function destroy(int $registrations) : Response
+    {
+        if (Registration::findOrFail($registrations)->delete())
+            return response()->noContent();
+        abort(404);
+    }
+
+    public function count() : int
+    {
+        return Registration::count();
     }
 }
