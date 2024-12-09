5382beb0c2e9a6d4bf949dc6c78c2452b0fa143a
SzokolAttila <szokol.ati2005@gmail.com>
2024-12-09 12:59:03 +0000

    added resource and model

--- backend/app/Http/Controllers/RegistrationController.php
+++ backend/app/Http/Controllers/RegistrationController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\RegistrationResource;
+use App\Models\Registration;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
 
 class RegistrationController extends Controller
 {
-    //
+    public function index() : JsonResource
+    {
+        return RegistrationResource::collection(Registration::all());
+    }
 }
